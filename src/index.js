6e550a8a93954ca239fa80dcd8e1dceedaedfe12
ofemirachael <ofemiracheal95@gmail.com>
2021-02-06 15:06:30 +0100

    SCA Task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 // complete the function
 function palindrom(str) {
-  // code goes here
+  var teststring = inputstring.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'');
+ var countstring = 0;
+ if(teststring==="") {
+   console.log(" there are no string here");
+   return false;
+ }
+ if ((teststring.length) % 2 === 0) {
+   countstring = (teststring.length) / 2;
+ } else {
+   if (teststring.length === 1) {
+     return 'It is a palindrome';
+   } else {
+     countstring = (teststring.length - 1) / 2;
+   }
+ }
+ for (var x = 0; x < countstring; x++) {
+   if (teststring[x] != teststring.slice(-1-x)[0]) {
+     return 'It is not a palindrome';;
+   }
+ }
+ return 'It is a palindrome';;
 }
 
 function solution(arg) {
